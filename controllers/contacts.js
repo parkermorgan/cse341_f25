edb98d16ddf2ae38e4ddccf8c5e2dbd832f16e6b
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-09-27 21:20:09 -0600

    finished assignment 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,25 +1,29 @@
 const { ObjectId } = require('mongodb');
-const client = require('../db/connection');
+const mongodb = require('../db/connection');
 
 const getAllContacts = async (req, res, next) => {
-  const result = await client.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb.getDb().collection('contacts').find();
+    const lists = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
-  })
+  } catch (err) {
+    next(err);
+  }
 };
 
 const getSingleContact = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await client
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const contact = await mongodb.getDb().collection('contacts').findOne({ _id: userId });
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists [0]);
-  })
+    res.status(200).json(contact);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = { getAllContacts, getSingleContact };
