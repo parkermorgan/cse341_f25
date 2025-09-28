eb456c3c1780262c0d84fa97f1c8e9b94d5cb718
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-09-27 20:07:14 -0600

    Assignment 2 progress

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
+const { connectToDb } = require('./db/connection')
 
 app.use('/', require('./routes'));
 
